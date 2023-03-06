9637789116a6f2f87bd83ea8d6b15d0a47af1861
PrabhaviHewage <prabhavihewage5@gmail.com>
2023-03-06 21:21:23 +0530

    first commit

---
+++ contracts/Greeting.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract Greeting {
+    string public name;
+    string public greetingPrefix = "Hello ";
+
+    constructor(string memory initialName) {
+        name = initialName;
+    }
+
+    function setName(string memory newName) public {
+        name = newName;
+    }
+
+    function getGreeting() public view returns (string memory) {
+        return string(abi.encodePacked(greetingPrefix, name));
+    }
+}
